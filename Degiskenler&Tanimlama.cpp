13303cdb3cf9ff38659621f2d6badb45bcbb87e7
Ömer Faruk Ballı <71135790+omerfarballi@users.noreply.github.com>
2021-03-07 17:20:43 +0300

    Add files via upload

---
+++ Degiskenler&Tanimlama.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a = 20;
+    a = 50;
+    
+    cout << "Bilge"<< a << endl;
+    
+    int b = 30;
+    int c = 40;
+    int x = 1, y = 2, z;
+    cout <<  b << endl;
+    cout <<  c << endl;
+    cout <<  x << endl;
+    cout <<  y << endl;
+    cout <<  z << endl;
+    return 0;
+}
---
+++ Degiskenİsimleri&Belirleyiciler.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ay;
+    
+    int yil;
+    
+    int gun;
+    
+    int int;
+    int cout ;
+    
+    int yil2021;      
+    
+    int 2yil; 
+    //başına sayı koyduğumuz zaman c++'ın bu kodu doğru anlayıp anlamaması belirli olmadığı için sayı ile başlamaması gerekmektedir.
+    
+    int _altcizgi ;   //özel semboller
+    
+    return 0;
+}
